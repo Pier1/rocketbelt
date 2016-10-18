ab625246403afd1accaad2f6e0df2f6c9ce3ac59
pierfw1\jmkysar <jmkysar@pier1.com>
2016-10-18 13:31:24 -0500

    Doc Styling + Image Handling

--- templates/js/site.js
+++ templates/js/site.js
@@ -11,4 +11,10 @@
       console.error('Action:', e.action);
       console.error('Trigger:', e.trigger);
   });
+
+  $(document).ready(function(){
+  	var pageTitle = document.title.toLowerCase().replace(' | slipway pattern library', '').replace(/\s+/g,'-');
+  	$('li.isActive', '#docs-leftnav').removeClass('isActive');
+  	$('#docs-leftnav').find('li[ref="' + pageTitle + '"]').addClass('isActive');
+  })
 })();
