3378ef79f5a8ca3a8d09d2d8d581811830a72794
Kendrick Arnett <kmarnett@gmail.com>
2016-10-28 15:13:03 -0500

    feat(Left nav): Overhaul left nav.
    Huge overhaul of the left nav. Now it's responsive and has a bit of eye candy.

--- templates/js/site.js
+++ templates/js/site.js
@@ -19,5 +19,14 @@
               .replace(/\s+/g,'-');
     $('li.isActive', '#docs-leftnav').removeClass('isActive');
   	$('#docs-leftnav').find('li[ref="' + pageTitle + '"]').addClass('isActive');
+
+    $('.nav .category-label').click(function (e) {
+      // Mobile heuristic. 48rem == 'md' breakpoint.
+      if (!window.matchMedia('(min-width: 48rem)').matches) {
+        e.preventDefault();
+        $('.nav .category-toggle').prop('checked', false);
+        $(this).siblings('.category-toggle').prop('checked', true);
+      }
+    });
   })
 })();
