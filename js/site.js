7d2b4b42379e8cb3030ca4eabc1c32368f0498f8
Kendrick Arnett <kmarnett@gmail.com>
2016-11-01 13:44:48 -0500

    Build for gh-pages: 37216ac44242124de02f9527b0fde77b8f90b694

---
+++ js/site.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  var clipboard = new Clipboard('.copy-code');
+
+  clipboard.on('success', function(e) {
+      e.clearSelection();
+  });
+
+  clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+  });
+
+  $(document).ready(function(){
+  	var pageTitle = document.title.toLowerCase()
+              .replace(' | rocketbelt pattern library', '')
+              .replace(/\ & /g," ")
+              .replace(/\s+/g,'-');
+    $('li.isActive', '#docs-leftnav').removeClass('isActive');
+  	$('#docs-leftnav').find('li[ref="' + pageTitle + '"]').addClass('isActive');
+
+    $('.nav .category-label').click(function (e) {
+      // Mobile heuristic. 48rem == 'md' breakpoint.
+      if (!window.matchMedia('(min-width: 48rem)').matches && $(this).siblings('.category-toggle').prop('checked') === false) {
+        e.preventDefault();
+        $('.nav .category-toggle').prop('checked', false);
+        $(this).siblings('.category-toggle').prop('checked', true);
+      }
+    });
+  })
+})();
