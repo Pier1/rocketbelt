2cbfbe0695cf303cbf4f5427361c38239f124b05
Kendrick Arnett <kmarnett@gmail.com>
2016-11-01 15:40:05 -0500

    Build for gh-pages: 8684ac7b26163d50dc1f321c78b3f0b932de02f8

---
+++ js/site.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  window.rocketbelt = window.rocketbelt || {};
+  window.rocketbelt.getScript = function (url, cb) {
+    var body = document.getElementsByTagName('body')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    script.onreadystatechange = cb;
+    script.onload = cb;
+
+    body.appendChild(script);
+  };
+
+  $(document).ready(function(){
+    var clipboard = new Clipboard('.copy-code');
+
+    clipboard.on('success', function(e) {
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+
+  	var pageTitle = document.title.toLowerCase()
+              .replace(' | rocketbelt pattern library', '')
+              .replace(/\ & /g," ")
+              .replace(/\s+/g,'-');
+    $('li.isActive', '#docs-leftnav').removeClass('isActive');
+  	$('#docs-leftnav').find('li[ref="' + pageTitle + '"]').addClass('isActive');
+
+    $('.nav .category-label').click(function (e) {
+      // Mobile heuristic. 48rem == 'md' breakpoint.
+      if (!window.matchMedia('(min-width: 48rem)').matches && $(this).siblings('.category-toggle').prop('checked') === false) {
+        e.preventDefault();
+        $('.nav .category-toggle').prop('checked', false);
+        $(this).siblings('.category-toggle').prop('checked', true);
+      }
+    });
+  });
+})();
