2c933f9a719d4a1f9cd93ccb88e5305aa7b0d712
pierfw1\jmkysar <jmkysar@pier1.com>
2016-11-01 20:14:08 -0500

    Added playground plugin, css, mixins and an example page.

--- templates/js/site.js
+++ templates/js/site.js
@@ -1,3 +1,4 @@
+var rb = rb || {};
 (function () {
   'use strict';
 
@@ -12,6 +13,21 @@
       console.error('Trigger:', e.trigger);
   });
 
+  var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
   $(document).ready(function(){
   	var pageTitle = document.title.toLowerCase()
               .replace(' | rocketbelt pattern library', '')
@@ -19,5 +35,43 @@
               .replace(/\s+/g,'-');
     $('li.isActive', '#docs-leftnav').removeClass('isActive');
   	$('#docs-leftnav').find('li[ref="' + pageTitle + '"]').addClass('isActive');
-  })
+
+    // Play Gifs
+    $('img.gipho').on('click', function(){
+      var $img = $(this), 
+        playSrc = $img.attr('data-play'),
+          rand = ( playSrc.indexOf('?') > -1 ? '&' : '?' ) + (Math.random() * 1000|0);
+      $img.attr('src', playSrc + rand );
+    });
+  });
+
+  $.playground.defaultOptions.wrapper = '.playground'
+
+  function launchPlayground(){
+    $('.playground-range').playground();
+    $('body').on('playgroundUpdated', '.playground-range', function(){
+      var $rangeInput = $(this),
+          base = $rangeInput.data('playground'),
+          $playground = $rangeInput.closest('.playground'),
+          $codeEl, targetHtmlStr;
+
+      if ( !$playground.length ) $playground = $rangeInput.closest('article');
+      $codeEl = $playground.find('.exampleWithCode code');
+          
+      if ( $playground.find('.copyable').length ) {
+        targetHtmlStr = $playground.find('.copyable')[0].outerHTML;
+      } else if ( $playground.find('.copyable-inner').length ) {
+        targetHtmlStr = $playground.find('.copyable-inner').html();
+      } else {
+        targetHtmlStr = base.$targetEl[0].outerHTML;
+      }
+      
+      $codeEl.html(escapeHtml(targetHtmlStr));
+      Prism.highlightElement($codeEl[0]);
+
+    });
+    $('.playground-range').trigger('input');
+  }
+
+  rb.launchPlayground = launchPlayground;
 })();
