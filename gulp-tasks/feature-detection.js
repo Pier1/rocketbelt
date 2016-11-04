3a75697d604e40138c2bc4c3fe410880c1177a4d
Kendrick Arnett <kmarnett@gmail.com>
2016-11-04 17:14:45 -0500

    feat(Build): Huge Gulpfile refactor! 😵
    Gulpfile was getting huge. Split tasks into individual files, which makes it much easier to see
    
    which tasks exist and which tasks depend on others.
    
    Fixes #30.

---
+++ gulp-tasks/feature-detection.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  module.exports = function (gulp, plugins, config) {
+    return function () {
+      return gulp.src([config.patternsPath + '/**/*.js',
+                       config.patternsPath + '/**/*.scss',
+                       '!' + config.patternsPath + '/base/feature-detection/*.js'])
+        .pipe(plugins.modernizr('rocketbelt.feature-detection.js',
+          {
+            options: [
+              'setClasses'
+            ],
+            tests: [
+              'touchevents'
+            ]
+          }))
+        .pipe(gulp.dest(config.patternsPath + '/base/feature-detection'))
+      ;
+    };
+  };
+})();
