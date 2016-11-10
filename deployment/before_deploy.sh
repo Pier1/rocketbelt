badc37dd9e2fd8a60bbdb6358ce34ff2a14a4b40
Kendrick Arnett <kmarnett@gmail.com>
2016-11-10 16:09:02 -0600

    ci(Deployment): Add granular deployment tasks.
    Breaks .travis.yml into smaller pieces so that branches don't fail the build.

---
+++ deployment/before_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+mv dist/* .
+rmdir dist
+
+git add . --all
+git commit -m "Build for gh-pages: ${SHA}"
+
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git merge -s recursive -X theirs TEMP_BRANCH -m "Merge into gh-pages: ${SHA}"
+git status --porcelain | awk '{if ($1=="DU") print $2}' | xargs git rm
+git commit -m "Merge into gh-pages: ${SHA}"
+
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push $SSH_REPO $TARGET_BRANCH
+git branch -D TEMP_BRANCH
