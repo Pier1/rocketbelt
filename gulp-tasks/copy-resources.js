331953259bf83fa46deb7b850e602f5a5d39fdbe
Kendrick Arnett <kmarnett@gmail.com>
2016-11-15 14:54:46 -0600

    feat(Typography): Update headers. Add font downloads.
    Include selected weights/variants for Brand Sans & Brand Serif. Updated Typography headers.

--- gulp-tasks/copy-resources.js
+++ gulp-tasks/copy-resources.js
@@ -3,7 +3,11 @@
 
   module.exports = function (gulp, plugins, config) {
     return function () {
-      return plugins.vinylFs.src([config.templatesPath + '/**/img/*.*', config.templatesPath + '/**/*.svg'])
+      return plugins.vinylFs.src([
+        config.templatesPath + '/**/img/*.*',
+        config.templatesPath + '/**/*.svg',
+        config.templatesPath + '/**/*.zip'
+      ])
         .pipe(plugins.changed(config.buildPath))
         .pipe(plugins.vinylFs.dest(config.buildPath, { overwrite: true }));
     };
