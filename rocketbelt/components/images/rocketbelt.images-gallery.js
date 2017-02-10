015fc05370f515ea381b5e3a9af8668ef28205e5
Kendrick Arnett <kmarnett@gmail.com>
2017-02-10 17:37:42 -0600

    feat(Images): Update gallery behavior.

--- rocketbelt/components/images/rocketbelt.images-gallery.js
+++ rocketbelt/components/images/rocketbelt.images-gallery.js
@@ -5,12 +5,12 @@
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.0/js/swiper.jquery.js',
       function () {
         var galleryTop =
-          new Swiper('.gallery-top',
+          new Swiper('.gallery-main',
             {
               nextButton: '.swiper-button-next',
               prevButton: '.swiper-button-prev',
-              spaceBetween: 10,
-              freeModeMomentum: false
+              observer: true,
+              observeParents: true
             }
           )
         ;
@@ -21,24 +21,44 @@
               spaceBetween: 10,
               centeredSlides: true,
               slidesPerView: 'auto',
-              touchRatio: 0.4,
               slideToClickedSlide: true,
               freeMode: true,
-              freeModeMomentumRatio: 0.4,
-              freeModeMomentumVelocityRatio: 1,
-              freeModeMomentumBounceRatio: 1
+              freeModeMomentumRatio: 0.6,
+              freeModeMomentumVelocityRatio: 0.8,
+              observer: true,
+              observeParents: true
             }
           )
         ;
 
         galleryTop.params.control = galleryThumbs;
-        // galleryThumbs.params.control = galleryTop;
+        galleryThumbs.slides[0].classList.add('swiper-slide-selected');
+
+        galleryThumbs.on('onTap', function (e) {
+          if (e.clickedIndex !== undefined && e.previousIndex !== e.clickedIndex) {
+            var len = e.slides.length;
+            for (var i = 0; i < len; i++) {
+              e.slides[i].classList.remove('swiper-slide-selected');
+            }
+
+            e.slides[e.clickedIndex].classList.add('swiper-slide-selected');
+            galleryTop.slideTo(e.clickedIndex);
+          }
+        });
 
         galleryThumbs.on('onSlideChangeStart', function (e) {
-          console.log('onSlideChangeStart');
           if (galleryTop.activeIndex !== e.activeIndex) {
             galleryTop.slideTo(e.clickedIndex);
           }
+        });
+
+        galleryTop.on('onSlideChangeEnd', function (e) {
+          var len = galleryThumbs.slides.length;
+          for (var i = 0; i < len; i++) {
+            galleryThumbs.slides[i].classList.remove('swiper-slide-selected');
+          }
+
+          galleryThumbs.slides[e.activeIndex].classList.add('swiper-slide-selected');
         });
       }
     );
