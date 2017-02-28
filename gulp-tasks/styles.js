349ba7ef3761070ff0d5dbadbef8a58c39dd6277
Kendrick Arnett <kmarnett@gmail.com>
2017-02-28 15:48:56 -0600

    fix(Build): Stop cssnano from removing needed vendor prefixes.
    cssnano was removing `-webkit` properties that were needed to support older versions of iOS. This
    
    commit passes a list of supported browsers to cssnano so that such prefixed properties make it into
    
    the nano-ified CSS.

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -3,18 +3,21 @@
 
   module.exports = function (gulp, plugins, config) {
     return function () {
+      var supported =
+        ['last 10 Chrome versions',
+         'last 10 Firefox versions',
+         'Safari >= 9',
+         'ie >= 9',
+         'Edge >= 1',
+         'iOS >= 8',
+         'Android >= 4.4'];
+
       var source = gulp.src([config.patternsPath + '/**/*.scss', config.templatesPath + '/scss/**/*.scss'])
         .pipe(plugins.plumber({ errorHandler: plugins.notify.onError('Error: <%= error.message %>') }))
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.sass())
         .pipe(plugins.autoprefixer({
-          browsers: ['last 10 Chrome versions',
-                     'last 10 Firefox versions',
-                     'Safari >= 9',
-                     'ie >= 9',
-                     'Edge >= 1',
-                     'iOS >= 8',
-                     'Android >= 4.4']
+          browsers: supported
         }))
         .pipe(plugins.postcss([plugins.postcssFlexibility()]))
       ;
@@ -27,7 +30,7 @@
       var min = source.pipe(plugins.clone())
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.rename({ suffix: '.min' }))
-        .pipe(plugins.postcss([plugins.cssnano()]))
+        .pipe(plugins.postcss([plugins.cssnano({ autoprefixer: { browsers: supported } })]))
         .pipe(plugins.size(config.sizeOptions))
         .pipe(plugins.sourcemaps.write('.', { sourceRoot: null }))
         .pipe(gulp.dest(config.buildCssPath))
