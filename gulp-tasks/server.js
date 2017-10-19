3bd595a24eea506161c8391fdf03086f73165207
Kendrick Arnett <kmarnett@gmail.com>
2017-10-19 16:14:38 -0500

    build(BrowserSync): Update BrowserSync config to prevent multiple reloads.

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -4,6 +4,7 @@
   module.exports = function (gulp, plugins, config) {
     return function () {
       return plugins.browserSync.init({
+        reloadDebounce: 1000,
         server: {
           injectChanges: true,
           baseDir: config.buildPath
