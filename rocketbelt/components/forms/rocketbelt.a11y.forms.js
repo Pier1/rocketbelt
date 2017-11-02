895b43f9e3aedd4d6055fb21ee3faaa743210e65
Kendrick Arnett <kmarnett@gmail.com>
2017-11-02 14:04:00 -0500

    WIP.

--- rocketbelt/components/forms/rocketbelt.a11y.forms.js
+++ rocketbelt/components/forms/rocketbelt.a11y.forms.js
@@ -1,24 +1,41 @@
-(function () {
-  function generateShortId() {
-    // Break the id into 2 parts to provide enough bits to the random number.
-    // This should be unique up to 1:2.2 bn.
-    var firstPart = (Math.random() * 46656) | 0;
-    var secondPart = (Math.random() * 46656) | 0;
-    firstPart = ('000' + firstPart.toString(36)).slice(-3);
-    secondPart = ('000' + secondPart.toString(36)).slice(-3);
-    return firstPart + secondPart;
-  }
-
+(function rocketbeltA11yForms(window, document) {
   function decorateInputs() {
     var formEls = document.querySelectorAll('.form-group input, .form-group select, .form-group textarea, .form-group fieldset');
     var formElsLen = formEls.length;
 
     for (var i = 0; i < formElsLen; i++) {
       var formEl = formEls[i];
-      var siblings = Array.prototype.filter.call(formEl.parentNode.children, function (child) {
+
+      var siblings = Array.prototype.filter.call(formEl.parentNode.children, function isSibling(child) {
         return child !== formEl;
       });
       var sibLen = siblings.length;
+
+      // Set an observer to listen for .invalid.
+      var observer = new MutationObserver(function onMutation(mutations) {
+        var mutationsLen = mutations.length;
+
+        for (var k = 0; k < mutationsLen; k++) {
+          var mutation = mutations[k];
+          // if oldvalue !== invalid && class list contains invalid
+          if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+            if (mutation.oldValue !== 'invalid' && mutation.target.classList.contains('invalid')) {
+              console.dir(mutation);
+              // Do the decoratin'
+              if (!formEl.hasAttribute('aria-invalid')) {
+                formEl.setAttribute('aria-invalid', 'true');
+              }
+                // role='alert' & aria-live='polite' on the message
+            } else if (mutation.oldValue !== 'invalid' && !mutation.target.classList.contains('invalid')) {
+                    // remove aria-invalid, role, aria-live
+            }
+          }
+        }
+      });
+
+      observer.observe(formEl, { attributes: true, attributeOldValue: true });
+
+//
 
       for (var j = 0; j < sibLen; j++) {
         var thisSib = siblings[j];
@@ -27,7 +44,7 @@
           // Don't clobber any existing attributes!
           if (!formEl.hasAttribute('aria-describedby') && !thisSib.id) {
             // Decorate input & this sibling
-            var id = 'rb-a11y-' + generateShortId();
+            var id = 'rb-a11y-' + window.rb.getShortId();
             formEl.setAttribute('aria-describedby', id);
             thisSib.id = id;
           }
@@ -36,12 +53,5 @@
     }
   }
 
-  if (
-    document.readyState === 'complete' ||
-    (document.readyState !== 'loading' && !document.documentElement.doScroll)
-  ) {
-    decorateInputs();
-  } else {
-    document.addEventListener('DOMContentLoaded', decorateInputs);
-  }
-})();
+  window.rb.onDocumentReady(decorateInputs);
+})(window, document);
