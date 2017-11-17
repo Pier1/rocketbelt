cb5c44aeeded6932345683a4ccb97246c6531078
Kendrick Arnett <kmarnett@gmail.com>
2017-11-17 13:19:36 -0600

    fix(Progress Indicators): Add aria-disabled to link for current step. Factor out aria magic strings.

--- rocketbelt/components/progress/rocketbelt.a11y.progress-stepped.js
+++ rocketbelt/components/progress/rocketbelt.a11y.progress-stepped.js
@@ -1,5 +1,5 @@
-(function rocketbeltA11yProgressStepped(window, document) {
-  var ARIA_CURRENT = 'aria-current';
+(function rocketbeltA11yProgressStepped(rb, document) {
+  var aria = rb.aria;
 
   function onAttrMutation(mutations) {
     var mutationsLen = mutations.length;
@@ -8,21 +8,21 @@
       var mutation = mutations[i];
       var attributes = mutation.target.attributes;
 
-      var hasAriaCurrent = attributes.getNamedItem(ARIA_CURRENT) ? true : false;
+      var hasAriaCurrent = attributes.getNamedItem(aria.current) ? true : false;
       // If target's attributes include aria-current
       if (hasAriaCurrent) {
         // Decorate current & subsequent links with aria-disabled
-        var linksToDisable = mutation.target.parentNode.querySelectorAll('[' + ARIA_CURRENT + '] a, [' + ARIA_CURRENT + '] ~ li a');
+        var linksToDisable = mutation.target.parentNode.querySelectorAll('[' + aria.current + '] a, [' + aria.current + '] ~ li a');
         var linksLen = linksToDisable.length;
 
         for (var j = 0; j < linksLen; j++) {
           var link = linksToDisable[j];
-          link.setAttribute('aria-disabled', true);
+          link.setAttribute(aria.disabled, true);
         }
       } else {
       // If target doesn't have aria-current, remove aria-disabled from child link
         var linkToEnable = mutation.target.querySelector('a');
-        linkToEnable.removeAttribute('aria-disabled');
+        linkToEnable.removeAttribute(aria.disabled);
       }
     }
   }
@@ -34,21 +34,32 @@
     for (var i = 0; i < progressIndicatorsLen; i++) {
       var progressIndicator = progressIndicators[i];
 
-      if (!progressIndicator.hasAttribute('aria-label')) {
-        progressIndicator.setAttribute('aria-label', 'Progress Indicator');
+      if (!progressIndicator.hasAttribute(aria.label)) {
+        progressIndicator.setAttribute(aria.label, 'Progress Indicator');
       }
 
       progressIndicator.setAttribute('role', 'nav');
 
       var observer = new MutationObserver(function onmutate(mutations) { onAttrMutation(mutations); });
-      observer.observe(progressIndicator, { subtree: true, attributes: true, attributeOldValue: true, attributeFilter: ['aria-current'] });
+      observer.observe(progressIndicator, { subtree: true, attributes: true, attributeOldValue: true, attributeFilter: [aria.current] });
 
-      var current = progressIndicator.querySelector('[' + ARIA_CURRENT + ']');
+      var current = progressIndicator.querySelector('[' + aria.current + ']');
       if (!current) {
-        progressIndicator.querySelector('li').setAttribute(ARIA_CURRENT, 'page');
+        current = progressIndicator.querySelector('li');
+        current.setAttribute(aria.current, 'page');
+      }
+
+      current.querySelector('a').setAttribute(aria.disabled, 'true');
+
+      var sibs = progressIndicator.querySelectorAll('[' + aria.current + '] ~ li a');
+      var sibsLen = sibs.length;
+
+      for (var j = 0; j < sibsLen; j++) {
+        var sib = sibs[j];
+        sib.setAttribute(aria.disabled, 'true');
       }
     }
   }
 
   window.rb.onDocumentReady(decorateProgressStepped);
-})(window, document);
+})(window.rb, document);
