9aaa52ba008a9c932cd45912214054e45702f9f3
Kendrick Arnett <kmarnett@gmail.com>
2017-12-12 12:09:02 -0600

    Build for gh-pages: 89663af33993e3cecc14c3aee60e5762702ca7fa

---
+++ base/rocketbelt.js
@@ -0,0 +1,54 @@
+(function rocketbelt(window, document) {
+  window.rb = window.rb || {};
+
+  window.rb.getShortId = function getShortId() {
+    // Break the id into 2 parts to provide enough bits to the random number.
+    // This should be unique up to 1:2.2 bn.
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ('000' + firstPart.toString(36)).slice(-3);
+    secondPart = ('000' + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+  };
+
+  window.rb.onDocumentReady = function onDocumentReady(fn) {
+    if (
+      document.readyState === 'complete' ||
+      (document.readyState !== 'loading' && !document.documentElement.doScroll)
+    ) {
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  };
+
+  // Adds "more correct" CustomEvent support to IE >= 9
+  // See https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+  (function polyfillCustomEvent() {
+    if (typeof window.CustomEvent === 'function' ) return false;
+
+    function CustomEvent(event, params) {
+      params = params || { bubbles: false, cancelable: false, detail: undefined };
+      var evt = document.createEvent( 'CustomEvent' );
+      evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+      return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent;
+  })();
+
+  window.rb.aria = {
+    'current':     'aria-current',
+    'describedby': 'aria-describedby',
+    'disabled':    'aria-disabled',
+    'expanded':    'aria-expanded',
+    'haspopup':    'aria-haspopup',
+    'hidden':      'aria-hidden',
+    'invalid':     'aria-invalid',
+    'label':       'aria-label',
+    'labelledby':  'aria-labelledby',
+    'live':        'aria-live',
+    'role':        'role'
+  };
+})(window, document);
---
+++ components/buttons/rocketbelt.buttons-dropdown.js
@@ -0,0 +1,80 @@
+(function rocketbeltButtonsDropdown(rb, document) {
+  var aria = rb.aria;
+
+  function decorateDropdowns() {
+    var triggers = document.querySelectorAll('.button-dropdown_trigger');
+    var triggersLen = triggers.length;
+
+    var DEFAULT_BUTTON_SELECTOR = '.button, button';
+
+    var onChange = function onFocusIn(e) {
+      var input = e.target;
+      var checked = input.checked;
+      var ddContent = e.target.parentNode.querySelector('.button-dropdown_content');
+
+      if (checked) {
+        input.setAttribute(aria.expanded, true);
+        ddContent.setAttribute(aria.hidden, false);
+      } else {
+        input.setAttribute(aria.expanded, false);
+        ddContent.setAttribute(aria.hidden, true);
+      }
+    };
+
+    for (var i = 0; i < triggersLen; i++) {
+      var trigger = triggers[i];
+      var label = trigger.parentNode.querySelector('label');
+      var ddContent = trigger.parentNode.querySelector('.button-dropdown_content');
+      var defaultButton = trigger.parentNode.querySelector(DEFAULT_BUTTON_SELECTOR);
+      var ddLabelledBy = defaultButton.id ? defaultButton.id : 'rb-a11y_' + rb.getShortId();
+
+      var icon = trigger.parentNode.querySelector('.button-dropdown_icon .icon');
+      icon.setAttribute(aria.hidden, true);
+
+      var triggerId = trigger.id ? trigger.id : 'rb-a11y_' + rb.getShortId();
+      trigger.id = triggerId;
+      label.setAttribute('for', triggerId);
+
+      if (ddContent) {
+        defaultButton.id = ddLabelledBy;
+
+        trigger.setAttribute(aria.haspopup, true);
+        trigger.setAttribute(aria.expanded, false);
+        trigger.setAttribute(aria.label, 'Toggle Dropdown');
+
+        ddContent.setAttribute(aria.hidden, true);
+        ddContent.setAttribute(aria.labelledby, ddLabelledBy);
+      }
+
+      trigger.addEventListener('change', function change(e) { onChange(e); });
+    }
+
+    document.addEventListener('click', function onclick(e) {
+      var path = e.path;
+      var pathLen = path.length;
+      var shouldClose = true;
+
+      for (var j = 0; j < pathLen; j++) {
+        var el = path[j];
+
+        if (el.classList && (el.classList.contains('button-dropdown_content') || el.classList.contains('button-dropdown_icon') || el.classList.contains('button-dropdown_trigger'))) {
+          shouldClose = false;
+          break;
+        }
+      }
+
+      if (shouldClose) {
+        var ddTriggers = document.querySelectorAll('.button-dropdown_trigger');
+        var ddTriggersLen = ddTriggers.length;
+
+        for (var k = 0; k < ddTriggersLen; k++) {
+          ddTriggers[k].checked = false;
+        }
+      }
+    });
+  }
+
+  rb.onDocumentReady(decorateDropdowns);
+  rb.buttons = rb.buttons || {};
+  rb.buttons.decorateDropdowns = decorateDropdowns;
+})(window.rb, document);
