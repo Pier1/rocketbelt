f5e96b1b411fd05db03845dc504aaaf20ec001e1
Kendrick Arnett <kmarnett@gmail.com>
2017-12-19 16:20:29 -0600

    feat(Global): Include hover intent vendor script.

--- gulp-tasks/copy-js.js
+++ gulp-tasks/copy-js.js
@@ -3,6 +3,10 @@
 
   module.exports = function (gulp, plugins, config) {
     return function () {
+      plugins.vinylFs.src(['node_modules/hoverintent/dist/hoverintent.min.js'])
+        .pipe(plugins.rename('rocketbelt.hoverintent.js'))
+        .pipe(plugins.vinylFs.dest(config.patternsPath + '/base/vendor/'));
+
       return plugins.vinylFs.src([config.templatesPath + '/**/*.js'])
         .pipe(plugins.changed(config.buildPath))
         .pipe(plugins.vinylFs.dest(config.buildPath, { overwrite: true }));
