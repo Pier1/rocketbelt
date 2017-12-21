14b1b4d028e0f46c5bbce8a927ea164edba23437
Kendrick Arnett <kmarnett@gmail.com>
2017-12-21 16:02:19 -0600

    feat(Javascript): Update JS build tasks to accept ES6.

--- gulp-tasks/uglify.js
+++ gulp-tasks/uglify.js
@@ -8,6 +8,9 @@
         .pipe(plugins.plumber({ errorHandler: plugins.notify.onError('Error: <%= error.message %>') }))
         .pipe(plugins.rename({ suffix: '.min' }))
         .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.babel({
+          presets: ['env']
+        }))
         .pipe(plugins.uglify())
         .pipe(plugins.size(config.sizeOptions))
         .pipe(plugins.sourcemaps.write('.', { sourceRoot: '.' }))
