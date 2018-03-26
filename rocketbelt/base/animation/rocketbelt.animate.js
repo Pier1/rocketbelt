c5a74b9051ef9e6cbf997b42caaa3ebe7ad2da84
Kendrick Arnett <kmarnett@gmail.com>
2018-03-26 13:13:48 -0500

    refactor(Animation): Remove animate.css-based animations. Break up RB animations into partials.

--- rocketbelt/base/animation/rocketbelt.animate.js
+++ rocketbelt/base/animation/rocketbelt.animate.js
@@ -94,7 +94,9 @@
         el.classList.remove(...classesToRemove);
       }
 
-      callback();
+      if (callback) {
+        callback();
+      }
     };
 
     rb.animate.onAnimationEnd(el, cb, true);
@@ -102,16 +104,39 @@
 
   if ($) {
     const functions = {
+      /**
+       * Add a Rocketbelt animation to a jQuery object.
+       *
+       * @param  {string} animationName The Rocketbelt animation name.
+       * @param  {(object|function)} configOrCallback A configuration object or a callback to run after the animation finishes.
+       * @returns {object} A chainable jQuery object.
+       */
       'animate': function animate(animationName, configOrCallback) {
         return this.each(function eachAnimate() {
           return window.rb.animate.animate(this, animationName, configOrCallback);
         });
       },
+
+      /**
+       * Execute a function after a transition finishes.
+       *
+       * @param {function} callback The function to execute after the transition.
+       * @param {boolean} [once=false] Whether to execute the callback after subsequent transitions. If true, removes the event listener after the first execution of the callback.
+       * @returns {object} A chainable jQuery object.
+       */
       'animate.onTransitionEnd': function onTransitionEnd(callback, once = false) {
         return this.each(function eachTransitionEnd() {
           return window.rb.animate.onTransitionEnd(this, callback, once);
         });
       },
+
+      /**
+       * Execute a function after a transition finishes.
+       *
+       * @param {function} callback The function to execute after the transition.
+       * @param {boolean} [once=false] Whether to execute the callback after subsequent animations. If true, removes the event listener after the first execution of the callback.
+       * @returns {object} A chainable jQuery object.
+       */
       'animate.onAnimationEnd': function onAnimationEnd(callback, once = false) {
         return this.each(function eachAnimationEnd() {
           return window.rb.animate.onAnimationEnd(this, callback, once);
@@ -119,6 +144,12 @@
       }
     };
 
+    /**
+     * Rocketbelt helper functions.
+     *
+     * @param {any} functionAndOptions A Rocketbelt helper function name as a string.
+     * @return {any} The value returned by the passed function name.
+     */
     $.fn.rb = function $rb(functionAndOptions) {
       if (functions[functionAndOptions]) {
         return functions[functionAndOptions].apply(this, Array.prototype.slice.call(arguments, 1));
