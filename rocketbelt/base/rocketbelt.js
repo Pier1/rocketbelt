af388ab1298d6094c5f05cabd9acb884c7824c47
Kendrick Arnett <kmarnett@gmail.com>
2018-07-17 13:35:59 -0500

    feat(Global): Add `String.prototype.repeat` polyfill for legacy browser support.

--- rocketbelt/base/rocketbelt.js
+++ rocketbelt/base/rocketbelt.js
@@ -91,6 +91,47 @@
     });
   };
 
+  // Polyfill String.prototype.repeat for IE11. This block can be deleted when
+  // IE11 support is no longer needed in the future.
+  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+  (() => {
+    if (!String.prototype.repeat) {
+      /* eslint-disable */
+      String.prototype.repeat = function repeat(count) {
+        if (this == null) {
+          throw new TypeError('can\'t convert ' + this + ' to object');
+        }
+        var str = '' + this;
+        count = +count;
+        if (count != count) {
+          count = 0;
+        }
+        if (count < 0) {
+          throw new RangeError('repeat count must be non-negative');
+        }
+        if (count == Infinity) {
+          throw new RangeError('repeat count must be less than infinity');
+        }
+        count = Math.floor(count);
+        if (str.length == 0 || count == 0) {
+          return '';
+        }
+        // Ensuring count is a 31-bit integer allows us to heavily optimize the
+        // main part. But anyway, most current (August 2014) browsers can't handle
+        // strings 1 << 28 chars or longer, so:
+        if (str.length * count >= 1 << 28) {
+          throw new RangeError('repeat count must not overflow maximum string size');
+        }
+        var rpt = '';
+        for (var i = 0; i < count; i++) {
+          rpt += str;
+        }
+        return rpt;
+      }
+    }
+    /* eslint-enable */
+  })();
+
   // Polyfill NodeList.forEach in IE (all versions) and Safari (pre-10).
   // See https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
   ((NodeListProto) => {
