f97e56b4d9b09021512c0363da75d341fa4faf63
Kendrick Arnett <kmarnett@gmail.com>
2019-10-25 14:25:43 -0500

    feat(Messages): Add updated message pattern. Update DeprecationNotice to use it.

--- src/components/deprecation-notice.js
+++ src/components/deprecation-notice.js
@@ -2,6 +2,8 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import RbIcon from './rb-icon';
+
 const DeprecationNotice = ({ alternativeComponents }) => {
   let hasAlternatives = false;
   let asAnAlternativeText = '';
@@ -15,42 +17,49 @@ const DeprecationNotice = ({ alternativeComponents }) => {
 
   return (
     <>
-      <aside>
-        This component is deprecated and may be removed in a future version of
-        Rocketbelt.
+      <aside className="message message-warning deprecation-notice">
+        <RbIcon icon="warning" className="message_icon" />
+        <section className="message_body">
+          <span className="message_title">
+            This component has been deprecated.
+          </span>
+          <span className="message_text">
+            It may be removed in a future version of Rocketbelt.
+            {hasAlternatives && (
+              <>
+                {` `}Consider the
+                {alternativeComponents.map((component, index) => {
+                  let separator =
+                    index === 0
+                      ? ' '
+                      : index + 1 === alternativeComponents.length
+                      ? ' or '
+                      : ', ';
+                  return component.uri ? (
+                    <>
+                      {separator}
+                      <Link
+                        className="deprecated-component-alternative"
+                        to={component.uri}
+                      >
+                        {component.name}
+                      </Link>
+                    </>
+                  ) : (
+                    <>
+                      {separator}
+                      <span className="deprecated-component-alternative">
+                        {component.name}
+                      </span>
+                    </>
+                  );
+                })}
+                {asAnAlternativeText}.
+              </>
+            )}
+          </span>
+        </section>
       </aside>
-      {hasAlternatives && (
-        <p>
-          Consider using the
-          {alternativeComponents.map((component, index) => {
-            let separator =
-              index === 0
-                ? ' '
-                : index + 1 === alternativeComponents.length
-                ? ' or '
-                : ', ';
-            return component.uri ? (
-              <>
-                {separator}
-                <Link
-                  className="deprecated-component-alternative"
-                  to={component.uri}
-                >
-                  {component.name}
-                </Link>
-              </>
-            ) : (
-              <>
-                {separator}
-                <span className="deprecated-component-alternative">
-                  {component.name}
-                </span>
-              </>
-            );
-          })}
-          {asAnAlternativeText}.
-        </p>
-      )}
     </>
   );
 };
