61b44c0b8dcdf0e623d15b92089cd9d50d7c5af7
Kendrick Arnett <kmarnett@gmail.com>
2020-02-06 15:27:01 -0600

    fix(Sample Site): Prevent duplicate injection of scripts for revisited pages.

--- src/utils/addScript.js
+++ src/utils/addScript.js
@@ -1,5 +1,11 @@
 exports.addScript = (src) => {
   if (typeof document !== 'undefined') {
+    const matchingScripts = document.querySelectorAll(`script[src*='${src}?'`);
+
+    matchingScripts.forEach((s) => {
+      s.remove();
+    });
+
     const s = document.createElement('script');
     s.setAttribute('src', `${src}?${Date.now()}`);
     document.body.appendChild(s);
