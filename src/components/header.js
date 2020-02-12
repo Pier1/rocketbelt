c661d6c8001bff2af5d33430be8a228378b132df
Kendrick Arnett <kmarnett@gmail.com>
2020-02-12 11:40:12 -0600

    feat(Sample Site): Update header styles.

--- src/components/header.js
+++ src/components/header.js
@@ -7,22 +7,19 @@ import Navigation from './navigation';
 import { css, jsx } from '@emotion/core';
 import { cx } from 'emotion';
 
-// import { color, media } from './rocketbelt.js';
-const breakpoints = [480, 768, 992, 1200];
-const media = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
-
-const toggleNav = () => {
-  document.querySelector('.rbio-header').classList.toggle('rbio-nav-open');
-};
+import { colors, media } from '../utils/rocketbelt';
 
 const headerCss = css`
-  background: #ced9e5;
+  background: white;
+  border-bottom: 3px solid ${colors.brand.prussianBlue};
   min-height: 44px;
   box-shadow: 0 1px 12px 0 rgba(0, 0, 0, 0.08);
   position: fixed;
+  padding: 0 0.25rem;
   width: 100%;
 
   ${media[0]} {
+    border: 3px solid ${colors.brand.prussianBlue};
     padding: 0 1rem;
     width: calc(100% - 8rem);
     max-width: calc(1024px - 8rem);
@@ -37,8 +34,10 @@ const headerCss = css`
       content: '';
       height: 2rem;
       position: fixed;
-      left: 0;
-      top: -2rem;
+      padding: 0 1rem;
+      left: -3px;
+      right: -3px;
+      top: calc(-2rem - 3px);
       background-image: linear-gradient(
         180deg,
         #ffffff 0%,
@@ -52,7 +51,6 @@ const headerCss = css`
         rgba(255, 255, 255, 0.58) 90%,
         rgba(255, 255, 255, 0.5) 100%
       );
-      width: 100%;
     }
   }
 `;
