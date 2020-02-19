353ee5edd825144fa76c239bbebd37ade141797a
Kendrick Arnett <kmarnett@gmail.com>
2020-02-19 14:05:55 -0600

    chore(Build): Remove vestigial gulp tasks & npm packages.

--- gulp-tasks/default.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-(() => {
-  module.exports = (gulp, plugins, config) => {
-    return (done) => {
-      return plugins.runSequence('build', ['server', 'watch'], done);
-    };
-  };
-})();
