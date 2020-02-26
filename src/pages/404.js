74a16a0fd4714b7f2307b06b1518289dbb10a348
Kendrick Arnett <kmarnett@gmail.com>
2020-02-26 15:11:37 -0600

    fix(Sample Site): Randomize properly.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => {
+  const [imageNum, setImageNum] = useState(Math.floor(Math.random() * 8));
+
+  useEffect(() => {
+    setImageNum(Math.floor(Math.random() * 8));
+  });
+
   return (
     <Layout>
       <SEO title="404: Not found" />
       <h1>Page Not Found</h1>
       <video width="100%" autoPlay loop muted>
-        <source
-          src={`/images/404-${Math.floor(Math.random() * 8)}.mp4`}
-          type="video/mp4"
-        />
+        <source src={`/images/404-${imageNum}.mp4`} type="video/mp4" />
       </video>
     </Layout>
   );
