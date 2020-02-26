551ae4ef152fce3ec171b9114f22e788aac15650
Kendrick Arnett <kmarnett@gmail.com>
2020-02-26 15:02:06 -0600

    fix(Sample Site): Randomize properly.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,15 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => {
-  const rand = Math.floor(Math.random() * 8);
-
   return (
     <Layout>
       <SEO title="404: Not found" />
       <h1>Page Not Found</h1>
       <video width="100%" autoPlay loop muted>
-        <source src={`/images/404-${rand}.mp4`} type="video/mp4" />
+        <source
+          src={`/images/404-${Math.floor(Math.random() * 8)}.mp4`}
+          type="video/mp4"
+        />
       </video>
     </Layout>
   );
