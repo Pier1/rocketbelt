0e721011f3264da6555c010dc1e73eee117c9af5
Kendrick Arnett <kmarnett@gmail.com>
2020-02-26 14:21:44 -0600

    feat(Sample Site): Update 404 content & styles.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const rand = Math.floor(Math.random() * 8);
 
-export default NotFoundPage
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>Page Not Found</h1>
+      <video width="100%" autoPlay loop muted>
+        <source src={`/images/404-${rand}.mp4`} type="video/mp4" />
+      </video>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
