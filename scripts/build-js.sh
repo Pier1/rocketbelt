e3737646fd19098f7f199cea2101d754801ed9df
Kendrick Arnett <kmarnett@gmail.com>
2020-03-19 11:22:50 -0500

    style(Make indentation consistent. Remove unused Babel config.):

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -3,7 +3,7 @@
 files=()
 
 while IFS= read -r -d $'\0'; do
- files+=("$REPLY")
+  files+=("$REPLY")
 done < <(find ./dist -name 'rocketbelt*js' ! -name '*slipsum*' ! -name '*.min.js' -print0)
 
 length=${#files[@]}
@@ -11,9 +11,9 @@ i=0
 
 for file in "${files[@]}"
 do
-   i=$((i+1))
-   min=$(echo $file | sed 's/.js/.min.js/g')
-   echo "($i/$length) $min"
-   npx babel $file --presets=@babel/preset-env --source-maps=inline | \
-     npx uglifyjs -c -m --source-map 'content=inline' -o $min
+  i=$((i+1))
+  min=$(echo $file | sed 's/.js/.min.js/g')
+  echo "($i/$length) $min"
+  npx babel $file --presets=@babel/preset-env --source-maps=inline | \
+    npx uglifyjs -c -m --source-map 'content=inline' -o $min
 done
