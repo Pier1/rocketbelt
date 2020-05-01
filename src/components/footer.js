964e6a78703c1c6a2bd1639081e894b82f4a097a
Kendrick Arnett <kmarnett@gmail.com>
2020-05-01 13:42:43 -0500

    improvement(Sample Site): Add meatball to footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,13 @@ import React from 'react';
 import { css, jsx } from '@emotion/core';
 
 import { fontSize } from '../utils/rocketbelt';
+import meatball from '../images/meatball.svg';
 
 const footerCss = css`
   position: relative;
   left: 50%;
   display: flex;
-  padding: 1rem;
+  padding: 2rem 1rem;
   width: 100vw;
   background: #e8edf3;
   color: #45474a;
@@ -27,11 +28,25 @@ const Footer = () => (
       css={css`
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
       `}
     >
-      Rocketbelt was collaboratively assembled at Pier 1 Imports and is
-      distributed under an MIT license. 🚀 © {new Date().getFullYear()} Pier 1
-      Imports.
+      <div className="footer_text">
+        Rocketbelt was collaboratively assembled at Pier 1 Imports and is
+        distributed under an MIT license. 🚀 © {new Date().getFullYear()} Pier 1
+        Imports.
+      </div>
+      <div
+        className="footer_logo"
+        css={css`
+          margin-top: 1rem;
+        `}
+      >
+        <a href="https://www.pier1.com/">
+          <img src={meatball} />
+        </a>
+      </div>
     </div>
   </footer>
 );
